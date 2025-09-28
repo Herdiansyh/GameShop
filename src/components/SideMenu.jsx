67d4db16864f1d19595b6f719d247da38b8e1a72
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-09-28 16:00:53 +0700

    fix bug

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,26 +1,22 @@
-import React, { act, useState } from "react";
+import React from "react";
 import "./sidemenu.css";
 import navListData from "../data/navListData";
 import NavListitem from "./NavListitem";
 import NavListSocial from "./NavListSocial";
 
 function SideMenu({ active }) {
-  const [navData, setnavData] = useState(navListData);
-
+  const navData = navListData;
   return (
     <div className={`sideMenu ${active ? "active" : ""}`}>
-      <a href="#" className="logo">
+      <a href="/" className="logo">
         <i className="bi bi-controller"></i>
         <span className="brand">Play</span>
       </a>
       <ul className="nav">
-        {navData.map(
-          (
-            item //lanssung gunakan () agar langsung return
-          ) => (
-            <NavListitem name={item.name} key={item._id} icon={item.icon} />
-          )
-        )}
+        {/* lanssung gunakan () agar langsung return */}
+        {navData.map((item) => (
+          <NavListitem name={item.name} key={item._id} icon={item.icon} />
+        ))}
       </ul>
       <ul className="social">
         <NavListSocial />
